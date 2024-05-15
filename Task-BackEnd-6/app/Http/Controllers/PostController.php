59d9b80912dedd6f12717f2bfda8efb6f0ffb278
mjd132 <1111222233334444accb@gmail.com>
2024-05-15 20:49:16 +0330

    Implement post controller

---
+++ Task-BackEnd-6/app/Http/Controllers/PostController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $page = $request->query('page', 1);
+        $posts = Post::latest()->paginate(10, page: $page);
+        return view('post.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
+    public function create()
+    {
+        return view('post.create');
+    }
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        Post::create($validatedData);
+
+        return redirect()->route('post.index')->with('success', 'Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return view('post.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        return view('post.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        $validatedData = $request->validate([
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        $post->update($validatedData);
+
+        return redirect()->route('post.index')->with('success', 'Post updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('post.index')->with('success', 'Post deleted.');
+    }
+}
